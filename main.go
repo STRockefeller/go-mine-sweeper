c011f93141a18fea926a554afdb7d520bd56f106
Rockefeller <rockefel@kenda.com.tw>
2023-12-19 09:47:54 +0800

    feat: select difficulty

--- main.go
+++ main.go
@@ -8,16 +8,31 @@ import (
 	"strings"
 
 	"github.com/STRockefeller/go-mine-sweeper/internal/sweeper"
+	"github.com/charmbracelet/huh"
 )
 
 func main() {
-	width, height, mines := 10, 10, 10
-	board := sweeper.NewBoard(width, height, mines)
+	var board *sweeper.Board
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[*sweeper.Board]().Title("Game Difficulty").Options(
+				huh.NewOption("Easy", sweeper.NewBoard(8, 8, 7)),
+				huh.NewOption("Medium", sweeper.NewBoard(10, 10, 15)),
+				huh.NewOption("Easy", sweeper.NewBoard(12, 12, 29)),
+			).Value(&board),
+		),
+	)
+	if err := form.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("there are %d mines in the board.\r\n", board.Mines)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	// Game loop
 	for !board.Display() {
-		fmt.Print("Enter your move (e.g., 'R 3 4' to reveal or 'F 2 1' to flag): ")
+		fmt.Print("Enter your move (e.g., 'R 3 4' to reveal or 'F 2 1' to flag) (0-indexed from the top-left corner): ")
 		scanner.Scan()
 		input := scanner.Text()
 
